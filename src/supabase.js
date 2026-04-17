e8e6daaa46bd0b839fcd8a7ba77e3b73c3f8e3e4
tomdavie <tom.davie@ioptassets.com>
2026-04-17 20:49:52 +0100

    move supabase keys to environment variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wbdeqdmfkjoahivopqtd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiZGVxZG1ma2pvYWhpdm9wcXRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxODI0MzAsImV4cCI6MjA5MTc1ODQzMH0._H8_ZYaWjgQ0-dPyrcculVnHTHSG0dm7VKlTgGK-Eew';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
